a2550229db8eb8d9ed83391f37be86ec4f056930
123soikot <162465476+123soikot@users.noreply.github.com>
2024-03-13 16:08:14 +0630

    Update help.js

--- scripts/commands/help.js
+++ scripts/commands/help.js
@@ -89,9 +89,8 @@ let path = __dirname + `/cache/help.png`;
   fs.writeFileSync(path, Buffer.from(image, "utf-8"));*/
 
 
-    const text = `╰➤ 𝗧𝗢𝗧𝗔𝗟 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 ${arrayInfo.length}\n𝗨𝗦𝗘【 ${prefix}Help 】𝐏𝐀𝐆𝐄 𝐍𝐎.\n╰➤ «𝐍𝐔𝐌𝐁𝐄𝐑 𝐎𝐅 𝐏𝐀𝐆𝐄» ${page}
-╰➤ https://www.facebook.com/Imon.132233`;
-    return api.sendMessage(`╭──«~»──CMD──«~»──╮\n\nPAGE ${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)}` + "\n" + msg + "\n" + text, threadID, async (error, info) => {
+    const text = `╰➤ 𝗧𝗢𝗧𝗔𝗟 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 ${arrayInfo.length}\n𝗨𝗦𝗘【 ${prefix}Help 】𝐏𝐀𝐆𝐄 ..  `;
+    return api.sendMessage(`╭──«~»──CMD──«~»──╮\n\nPAGE ${page}/${Math.ceil(https://www.facebook.com/mdsakhoyat.hosen.9.length/numberOfOnePage)}` + "\n" + msg + "\n" + text, threadID, async (error, info) => {
       if (autoUnsend) {
         await new Promise(resolve => setTimeout(resolve, delayUnsend * 100000));
         return api.unsendMessage(info.messageID);
